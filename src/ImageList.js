284f0d7090bc3785bce638e85086d8c40aa536c0
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-28 17:32:01 -0700

    fixed typo

--- src/ImageList.js
+++ src/ImageList.js
@@ -16,6 +16,7 @@ export default class ImageList extends Component {
                             })
                             .map(creature => {
                             return <ImageItem creature={creature} />
+
                             })
                         }
                     </ul>
